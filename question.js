f0f6ae4ea4c81b638d8fcd6c2edc7492ae9574fe
dhexmun <105861503+dhexmun@users.noreply.github.com>
2022-09-06 15:07:42 +0100

    Tempoary Commit

---
+++ question.js
@@ -0,0 +1,181 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        Options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        Options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        Options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        Options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        Options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Choose the correct HTML element for the largest heading:",
+        answer: "<h1>",
+        Options: [
+            "<h1>",
+            "<h6>",
+            "<head>",
+            "<heading>"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "Which character is used to indicate an end tag?",
+        answer: "/",
+        Options: [
+            "<!>",
+            "<*>",
+            "/",
+            ">"
+        ]
+    },
+
+    {
+        numb: 8,
+        question: "Who is making the Web standards?",
+        answer: "The World Wide Web Consortium",
+        Options: [
+            "Google",
+            "Mozilla",
+            "The World Wide Web Consortium",
+            "Microsoft"
+        ]
+    },
+
+    {
+        numb: 9,
+        question: "The HTML global attribute, 'contenteditable' is used to:",
+        answer: "Specify whether the content of an element should be editable or not",
+        Options: [
+            "Specifies a context menu for an element. The menu appears when a user right-clicks on the element",
+            "Specify whether the content of an element should be editable or not",
+            "Update content from the server",
+            "Return the position of the first found occurrence of content inside a string"
+        ]
+    },
+
+    {
+        numb: 10,
+        question: "In HTML, which attribute is used to specify that an input field must be filled out?",
+        answer: "required",
+        Options: [
+            "validate",
+            "formvalidate",
+            "placeholder",
+            "required"
+        ]
+    },
+
+    {
+        numb: 11,
+        question: "Which operator is used to assign a value to a variable?",
+        answer: "=",
+        Options: [
+            "x",
+            "-",
+            "=",
+            "*"
+        ]
+    },
+
+    {
+        numb: 12,
+        question: "Which CSS property controls the text size?",
+        answer: "font-size",
+        Options: [
+            "font-size",
+            "font-style",
+            "text-style",
+            "text-size"
+        ]
+    },
+
+    {
+        numb: 13,
+        question: "How do you make each word in a text start with a capital letter?",
+        answer: "text-transform:capitalize",
+        Options: [
+            "transform:capitalize",
+            "text-transform:capitalize",
+            "text-style:capitalize",
+            "You can't do that with CSS"
+        ]
+    },
+
+    {
+        numb: 14,
+        question: "What is the default value of the position property?",
+        answer: "static",
+        Options: [
+            "fixed",
+            "absolute",
+            "relative",
+            "static"
+        ]
+    },
+
+    {
+        numb: 15,
+        question: "Which event occurs when the user clicks on an HTML element?",
+        answer: "onclick",
+        Options: [
+            "onchange",
+            "onmouseclick",
+            "onmouseover",
+            "onclick"
+        ]
+    },
+];
